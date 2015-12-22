30e7a0a8d34595191d31792800abe0adb260de21
joserobson <robsbq@gmail.com>
2015-12-22 15:17:29 -0200

    add enums

---
+++ com-facil-modulo-global.domain/Enum/ESituacaoCliente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com_facil_modulo_global.domain.Enum
+{
+    public enum ESituacaoCliente
+    {
+        EmDia = 1,
+        EmDebito = 2,
+        EmCobranca = 3
+    }
+}
---
+++ com-facil-modulo-global.domain/Enum/ETipoContrato.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com_facil_modulo_global.domain.Enum
+{
+    public enum ETipoContrato
+    {
+        CarteiraAssinada = 1,
+        ContratoPJ = 2,
+        Experiencia = 3
+    }
+}
